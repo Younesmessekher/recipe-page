1c550e64107e654a4f53d38b522dec3cd7d2c13b
Younesmessekher <149335049+Younesmessekher@users.noreply.github.com>
2024-07-01 18:25:55 +0100

    Add files via upload

---
+++ Untitled-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+struct time
+{
+    int hours;
+    int minute;
+    int second;
+} ;
+ int main () {
+    struct time T;
+
+
+
+
+
+
+
+
+
+
+
+                      }   
